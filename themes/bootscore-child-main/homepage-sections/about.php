539ee38fc4dfaa196cc0ea0c2d6446eff34a9a34
INDRA NAND JHA <indranandjha1993@gmail.com>
2023-03-27 11:00:44 +0530

    UI enhance for about page, and sidebar

--- themes/bootscore-child-main/homepage-sections/about.php
+++ themes/bootscore-child-main/homepage-sections/about.php
@@ -1,28 +1,39 @@
 <section id="about" class="about">
     <div class="container">
-
         <div class="row content">
             <div class="col-lg-6">
-                <h2>Eum ipsam laborum deleniti velitena</h2>
-                <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assum perenda sruen jonee trave</h3>
+                <h2>Software Development Services</h2>
+                <h3>We Build Your Digital Solutions</h3>
             </div>
             <div class="col-lg-6 pt-4 pt-lg-0">
                 <p>
-                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum
+                    We are a software company that specializes in delivering customized software solutions for our
+                    clients. Our team of experts uses the latest agile development methodology to ensure that we develop
+                    high-quality software products that meet our clients' specific needs. Our services include software
+                    development, microservices, AI, ML, and more.
                 </p>
                 <ul>
-                    <li><i class="fa-regular fa-hand-point-right"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequa</li>
-                    <li><i class="fa-regular fa-hand-point-right"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
-                    <li><i class="fa-regular fa-hand-point-right"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in</li>
+                    <li><i class="fa-regular fa-hand-point-right"></i> Customized software development to meet your
+                        specific needs.
+                    </li>
+                    <li><i class="fa-regular fa-hand-point-right"></i> Agile development methodology to ensure
+                        high-quality software products.
+                    </li>
+                    <li><i class="fa-regular fa-hand-point-right"></i> Microservices development for scalability and
+                        flexibility.
+                    </li>
+                    <li><i class="fa-regular fa-hand-point-right"></i> AI and ML development to bring intelligence to
+                        your software products.
+                    </li>
                 </ul>
                 <p class="fst-italic">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                    magna aliqua.
+                    We work closely with our clients to understand their business requirements and develop software
+                    solutions that help them achieve their goals.
                 </p>
             </div>
         </div>
 
     </div>
 </section>
+
+
